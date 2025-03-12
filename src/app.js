7dbd9568f025f1b1242cb0d8d194d7c0fefd5a00
Shadan-rahman <Shadanrahman222@gmail.com>
2025-03-12 22:47:06 +0530

    Exploring GET, POST and DELETE

--- src/app.js
+++ src/app.js
@@ -2,17 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("hello hello hello ");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Shadan", lastName: "Rahman" });
 });
 
-app.use("/", (req, res) => {
-  res.send("hello from the server");
+app.post("/user", (req, res) => {
+  res.send("Data Saved Successfully in Database");
 });
 
-app.use("/test", (req, res) => {
-  res.send("hello from the test");
+app.delete("/user", (req, res) => {
+  res.send(" Data deleted Successfully");
 });
+
+// app.use("/hello", (req, res) => {
+//   res.send("hello hello hello ");
+// });
+
+// app.use("/test", (req, res) => {
+//   res.send("hello from the test");
+// });
+
+// app.use("/", (req, res) => {
+//   res.send("hello from the server");
+// });
 
 app.listen(3000, () => {
   console.log("Server is started runing successfully");
