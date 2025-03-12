c1c27ce6c8ceb1acad76a6fad647c9ae20806375
Shadan-rahman <Shadanrahman222@gmail.com>
2025-03-12 23:05:50 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,17 +2,17 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
+app.get("/user/:UserId/:name/:password", (req, res) => {
   res.send({ firstName: "Shadan", lastName: "Rahman" });
 });
 
-app.post("/user", (req, res) => {
-  res.send("Data Saved Successfully in Database");
-});
+// app.post("/user", (req, res) => {
+//   res.send("Data Saved Successfully in Database");
+// });
 
-app.delete("/user", (req, res) => {
-  res.send(" Data deleted Successfully");
-});
+// app.delete("/user", (req, res) => {
+//   res.send(" Data deleted Successfully");
+// });
 
 // app.use("/hello", (req, res) => {
 //   res.send("hello hello hello ");
